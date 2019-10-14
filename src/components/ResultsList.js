a70428505ba622c372f4fa310500d7f959d165a6
KainoaKeanaaina <kaikeanaaina@gmail.com>
2019-10-14 13:18:15 -1000

    rendering flatlist of results

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Flatlist } from 'react-native'
 
 const ResultsList = ({ title, results }) => {
     return (
         <View>
             <Text style={styles.title}>{title}</Text>
-            <Text>Results: {results.length} </Text>
+            <Flatlist
+                horizontal //this is same as horizontal={true}
+                data={results}
+                keyExtractor={result => result.id} //pick something from the data where it won't change after rerendered
+                renderItem={({ item }) => {   //item is each individual item from the array of data
+                    return <Text>{item.name}</Text>
+                }}
+            />
         </View>
     )
 }
